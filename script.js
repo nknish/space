fdfbf5697777f8598d8324b2943b4519d967286f
nknish <noahknish@gmail.com>
2024-09-28 19:47:13 -0400

    Actually draw planets and sun! yippee

--- script.js
+++ script.js
@@ -14,6 +14,8 @@ document.addEventListener("DOMContentLoaded", function () {
     7.527967853278865, 10.392179752102058, 12.24656278308326, 15.06620390144777,
     27.89380397149159, 37.8153408024, 53.54401553862019, 67.08000968992178,
   ];
+  const scale = aVals[aVals.length - 1] * 1.05;
+
   const kVals = [
     26.08797865515552, 10.216561475088758, 6.283185307179586,
     3.3403430660178555, 0.5297795368616852, 0.21327852366529484,
@@ -34,6 +36,13 @@ document.addEventListener("DOMContentLoaded", function () {
     "uranus",
     "neptune",
   ];
+
+  const canvas = document.getElementById("canvas");
+  const ctx = canvas.getContext("2d");
+
+  const height = canvas.height;
+  const width = canvas.width;
+  const pSize = 40;
 
   ellipse();
   findPositions();
@@ -61,88 +70,61 @@ document.addEventListener("DOMContentLoaded", function () {
   }
 
   function ellipse() {
-    /*const height = 100;
-		const width = 100;
-		const curr = document.getElementById(name);
-		curr.rx = Math.round(width * aVals[i] / aVals[aVals.length-1] / 2);
-		curr.ry = Math.round(height * bVals[i] / bVals[bVals.length-1] / 2);
-		curr.cx = width/2;
-		curr.cy = height/2;*/
-
-    let height = 400;
-    let width = 800;
-
-    const canvas = document.getElementById("canvas");
-    const ctx = canvas.getContext("2d");
-
     for (let i = 0; i < aVals.length; i++) {
       ctx.beginPath();
-      let radiusx = (width * aVals[i]) / aVals[aVals.length - 1] / 2;
-      let radiusy = (height * bVals[i]) / bVals[bVals.length - 1] / 2;
+      let radiusx = (width * aVals[i]) / scale / 2;
+      let radiusy = (height * bVals[i]) / scale / 2;
       ctx.ellipse(width / 2, height / 2, radiusx, radiusy, 0, 0, 2 * Math.PI);
       ctx.setLineDash([5, 3]);
       ctx.strokeStyle = "White";
       ctx.globalAlpha = 0.5;
       ctx.stroke();
     }
+    ctx.globalAlpha = 1;
   }
 
   function findPositions() {
     for (let i = 0; i < 8; i++) {
       position(names[i], 0);
     }
+    drawSun();
   }
 
   function position(planetName, time) {
     let t = time;
     let indx = getIndx(planetName);
-    let a = aVals[indx];
-    let b = bVals[indx];
+    let a = aVals[indx] / 2;
+    let b = bVals[indx] / 2;
     let k = kVals[indx];
     let theta = thetaVals[indx];
-    let x = a * Math.cos(k * t + 0);
-    let y = b * Math.sin(k * t + 0);
+    let x = a * Math.cos(k * t + theta);
+    let y = b * Math.sin(k * t + theta);
 
-    const canvas = document.getElementById("canvas");
-    const ctx = canvas.getContext("2d");
+    x = canvas.width / 2 + (x * canvas.width) / scale;
+    y = canvas.height / 2 - (y * canvas.height) / scale;
 
-    make_base();
+    x = x - pSize / 2;
+    y = y - pSize / 2;
 
-    function make_base() {
-      base_image = new Image();
-      base_image.src = "img/mars.png";
-      base_image.onload = function () {
-        ctx.drawImage(base_image, 50, 50, 50, 50);
-      };
-    }
+    let planet_image = new Image();
 
-    // const img = document.getElementById("sun-image");
-    // ctx.drawImage(img, 30, 30, 100, 100);
+    const imageName = "img/" + planetName + ".png";
 
-    // ctx.beginPath();
-    // ctx.ellipse(10, 10, 100, 100, 0, 0, 2 * Math.PI);
-    // ctx.setLineDash([5, 3]);
-    // ctx.strokeStyle = "White";
-    // ctx.stroke();
+    planet_image.src = imageName;
+    planet_image.onload = function () {
+      ctx.drawImage(planet_image, x, y, pSize, pSize);
+    };
+  }
 
-    // make_base();
+  function drawSun() {
+    let sunImage = new Image();
 
-    // function make_base() {
-    //   base_image = new Image();
-    //   base_image.src = "img/mars.png";
-    //   base_image.onload = function () {
-    //     ctx.drawImage(base_image, 20, 20, 100, 100);
-    //   };
-    // }
+    const x = canvas.width / 2 - pSize - (0.75 * canvas.width) / scale;
+    const y = canvas.height / 2 - pSize;
 
-    let image = document.getElementById(planetName);
+    sunImage.src = "img/sun.png";
+    sunImage.onload = function () {
+      ctx.drawImage(sunImage, x, y, pSize * 2, pSize * 2);
+    };
   }
 });
-
-// /*function ellipses() {
-// 	/*let names = ["mercury-o","venus-o","earth-o","mars-o","jupiter-o","saturn-o","unanus-o","neptune-o"]
-// 	for(let i = 0; i < 8; i++) {
-// 		ellipse(names[i], i);
-// 	}
-
-// }*/
